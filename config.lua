4134d5c828e0900524168c8d13f336660500fbd8
Team-mini <mini-dl@emeil.ir>
2016-04-21 22:21:12 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 return {
 	bot_api_key = '',
 	time_offset = 0,
-	admin = 23646077,
-	channel = '',
+	admin = 106630608,
+	channel = 'https://telegram.me/mini_team','https://telegran.me/minitanz
 	plugins = {
 		'onmessage.lua', --THIS HAVE TO BE THE FIRST: IF AN USER IS SPAMMING/IS BLOCKED, THE BOT WON'T GO THROUGH PLUGINS
 		'admin.lua',
